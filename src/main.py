a4bcb51314cf6ae8a8e0eb3bfc80ef986788718c
Rustamova Darina <darisha6602@gmail.com>
2023-12-08 22:02:43 +0300

    Add token with db

--- src/main.py
+++ src/main.py
@@ -8,11 +8,13 @@ from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import OAuth2PasswordRequestForm
 from passlib.hash import bcrypt
+from sqlmodel import Session, select
 
 from src.auth import auth
 from src.model import base_models
+from src.model.conn import engine
 
-app = FastAPI()
+app = FastAPI(debug=True)
 
 
 @app.post("/token", response_model=base_models.Token)
@@ -33,23 +35,30 @@ async def login_for_access_token(
     return {"access_token": access_token, "token_type": "bearer"}
 
 
-@app.get("/users/me/", response_model=base_models.User)
+@app.get("/users/me/", response_model=base_models.Users)
 async def read_users_me(
-    current_user: Annotated[base_models.User, Depends(auth.get_current_active_user)]
+    current_user: Annotated[base_models.Users, Depends(auth.get_current_active_user)]
 ):
     return current_user
 
 
 @app.get("/users/me/items/")
 async def read_own_items(
-    current_user: Annotated[base_models.User, Depends(auth.get_current_active_user)]
+    current_user: Annotated[base_models.Users, Depends(auth.get_current_active_user)]
 ):
     return [{"item_id": "Foo", "owner": current_user.username}]
 
 
-@app.post("/users/create", response_model=base_models.User)
+@app.post("/users/create")
 async def create_user(user: base_models.UserCreate):
-    if user.username not in base_models.fake_users_db.keys():
+    print(user)
+    with Session(engine) as session:
+        statement = select(base_models.Users).where(
+            base_models.Users.username == user.username
+        )
+        user = session.exec(statement).first
+
+    if user:
         user_data = user.model_dump()
         hashed_password = bcrypt.hash(user_data.pop("password"))
 
@@ -57,17 +66,23 @@ async def create_user(user: base_models.UserCreate):
             "username": user_data["username"],
             "full_name": user_data["full_name"],
             "email": user_data["email"],
+        }
+        new_pass_user = {
+            "username": user_data["username"],
             "hashed_password": hashed_password,
-            "disabled": False,
         }
 
-        base_models.fake_users_db[user_data["username"]] = new_user
-        return new_user
+        new_user = base_models.Users(**new_user)
+        new_pass_user = base_models.Passwords(**new_pass_user)
+
+        with Session(engine) as session:
+            session.add(new_user)
+            session.add(new_pass_user)
     else:
         raise HTTPException(status_code=404, detail="User already exist")
 
 
-@app.delete("/users/{username}", response_model=base_models.User)
+@app.delete("/users/{username}", response_model=base_models.Users)
 async def delete_user(username: str):
     if username in base_models.fake_users_db:
         deleted_user = base_models.fake_users_db.pop(username)
@@ -76,16 +91,16 @@ async def delete_user(username: str):
         raise HTTPException(status_code=404, detail="User not found")
 
 
-@app.get("/users", response_model=list[base_models.User])
+@app.get("/users", response_model=list[base_models.Users])
 async def get_all_users():
     return list(base_models.fake_users_db.values())
 
 
-@app.put("/users/{username}", response_model=base_models.User)
-async def update_user(username: str, new_user_data: base_models.User):
+@app.put("/users/{username}", response_model=base_models.Users)
+async def update_user(username: str, new_user_data: base_models.Users):
     if username in base_models.fake_users_db:
         user_dict = base_models.fake_users_db[username]
-        user = base_models.User(**user_dict)
+        user = base_models.Users(**user_dict)
 
         user.email = new_user_data.email
         user.full_name = new_user_data.full_name
@@ -99,7 +114,7 @@ async def update_user(username: str, new_user_data: base_models.User):
 
 @app.get("/user_reqs/items/id/", response_model=base_models.UserRequest)
 async def read_own_user_reqs_by_id(
-    current_user: Annotated[base_models.User, Depends(auth.get_current_active_user)],
+    current_user: Annotated[base_models.Users, Depends(auth.get_current_active_user)],
     id: str,
 ):
     user_req = base_models.fake_user_reqs_db.get(id)
@@ -110,7 +125,7 @@ async def read_own_user_reqs_by_id(
 
 @app.get("/user_reqs/items/me/", response_model=list[base_models.UserRequest])
 async def read_own_user_reqs(
-    current_user: Annotated[base_models.User, Depends(auth.get_current_active_user)],
+    current_user: Annotated[base_models.Users, Depends(auth.get_current_active_user)],
 ):
     filtered_dict = {
         key: value
