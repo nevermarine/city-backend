e03eee6a437b31d640072999c6221f99d5140cfb
Rustamova Darina <darisha6602@gmail.com>
2023-12-12 23:01:10 +0300

    Delete fake db

--- src/model/base_models.py
+++ src/model/base_models.py
@@ -9,29 +9,6 @@ from sqlmodel import Column, Enum, Field, Relationship, SQLModel
 
 config = dotenv.dotenv_values(".env")
 
-fake_user_reqs_db = {
-    "a5cf4789b6f74285b820e754f4ed65bb": {
-        "id": "a5cf4789b6f74285b820e754f4ed65bb",
-        "username": "maxim",
-        "message": "Как мне сняться с учета в военкомате?",
-        "status": "Pending",
-    },
-    "6196a6cd7a3b4d77a8964c8a5efad8ab": {
-        "id": "6196a6cd7a3b4d77a8964c8a5efad8ab",
-        "username": "darinka",
-        "message": "Где в городе находится центр госуслуг?",
-        "status": "Resolved",
-        "response": "Здравствуйте, Дарина! Ближайший центр госуслуг находится в г. Навои.",
-    },
-    "41694f46d89046f689fdb119ff62de23": {
-        "id": "41694f46d89046f689fdb119ff62de23",
-        "username": "maxim",
-        "message": "Сколько стоят жвачки по рублю?",
-        "status": "Resolved",
-        "response": "Здравствуйте, Максим! Жвачки по рублю стоят 2 рубля.",
-    },
-}
-
 
 class Token(BaseModel):
     access_token: str
